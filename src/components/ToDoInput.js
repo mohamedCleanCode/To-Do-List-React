d25e2c0dead317c2e5c3ac3cf4a7c56a69b767a5
mohamed <m635379@gmail.com>
2023-01-28 06:29:26 +0200

    add shortid library and add date and time to dom

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToDoInput = () => {
+  const date = new Date();
+  let ampm = date.getHours() >= 12 ? "pm" : "am";
+  const [text, setText] = useState("");
+
   return (
     <div className="ToDoInput">
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" placeholder="Add a task..."></input>
+        <input
+          type="text"
+          placeholder="Add a task..."
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></input>
         <button>I Got This!</button>
       </form>
-      <p>Date, Time</p>
+      <span style={{ display: "inline-block", marginRight: "10px" }}>
+        {date.toLocaleDateString()}
+      </span>
+      <span style={{ display: "inline-block", marginRight: "5px" }}>
+        {date.toLocaleTimeString() + ampm}
+      </span>
       <div>task</div>
     </div>
   );
